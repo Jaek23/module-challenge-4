06b36f0cae39d6142b162c2d9f27beba0e19d05e
jae kim <jaehkim23@gmail.com>
2023-10-03 00:36:21 -0500

    worked on JS file

--- assets/main.js
+++ assets/main.js
@@ -13,52 +13,95 @@ var questions = [
                  {text:"Strings", correct: true}, 
                  {text:"Objects", correct: false}, 
                  {text:"Arrays", correct: false}, 
-                 {text:"Boolean", correct: false}, 
-                ]
+                 {text:"Boolean", correct: false}
+        ]
     },
 
     {
         question:"A boolean data type can only eqaul to true/false values?",
-        answers:["True", "False"]
+        answers:[
+                {text:"True", correct: true},
+                {text:"False", correct: false}
+                ]
     },
 
     {
         question:"Which property is used to get the amount of items in an array?",
-        answers:["Concat", "Push", "Length", "Size"]
+        answers:[
+                 {text:"Concat", correct:false},
+                 {text:"Push", correct:false},
+                 {text:"Length", correct:true} ,
+                 {text:"Size", correct:false}
+                ]
     },
 
     {
         question:"Which method is used to merge two or more arrays?",
-        answers:["For Each", "Concat", "Slice", "Sort"]
+        answers:[
+                {text:"For Each", correct:false},
+                {text:"Concat", correct:true}, 
+                {text:"Slice", correct:false},
+                {text:"Sort", correct:false}
+                ]
     },
 
     {
         question:"Can array data types hold in other data types inside?",
-        answers:["True", "False"]
+        answers:["True",  "False"]
     },
+   
 ]
 
 //setting this variable to index 0 so the var currentQuestions is eqaul to questions[0]//
-var currentQuestionIndex = 0;
 
-function showQuestions(){
-    var questionText = document.createElement("h3");
-    questionText.setAttribute("style", "font-size:32px");
-    questionElement.appendChild(questionText)
-    var currentQuestion = questions[currentQuestionIndex];
-//this will now display the questions[].questions when you click the startButton//
-    questionText.innerHTML = currentQuestion.question;
+
+function startQuiz(){
     startButton.style.display = "none"
     mainHeader.style.display = "none"
     instructions.style.display = "none"
-
-    for (var i=0; i <currentQuestion.answers.length; i++){
-        var answerButton = document.createElement("button");
-        answerElement.appendChild(answerButton)
-        answerButton.innerHTML = currentQuestion.answers.text
-    
-    }
-
+    showQuestionAndAnswers();
 }
 
-startButton.addEventListener("click", showQuestions);
+var currentQuestionIndex = 0;
+function showQuestionAndAnswers(){
+  var currentQuestion = questions[currentQuestionIndex];
+  questionElement.innerHTML = currentQuestion.question;
+  //resetState()
+  currentQuestion.answers.forEach(answer=>{
+    var answerList = document.createElement("li");
+    answerElement.appendChild(answerList);
+    var answerButton = document.createElement("button");
+    answerList.appendChild(answerButton);
+    answerButton.innerHTML = answer.text;
+    answerButton.addEventListener("click", function(){
+    resultMessage()
+    resetState()
+    if (currentQuestionIndex < questions.length){
+        currentQuestionIndex++  
+        showQuestionAndAnswers();
+    }
+    
+})
+
+  })
+}
+
+function resetState(){
+    questionElement.removeChild(questionElement.firstChild)
+    while(answerElement.firstChild){
+        answerElement.removeChild(answerElement.firstChild)
+    }
+}
+
+function resultMessage(){
+
+
+    
+}
+
+startButton.addEventListener("click", startQuiz);
+
+
+
+
+   
