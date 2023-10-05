e1a3aee2b20fd02438c55490cb5ec400519522e2
jae kim <jaehkim23@gmail.com>
2023-10-04 23:11:22 -0500

    fixed event listner on JS file

--- assets/main.js
+++ assets/main.js
@@ -3,13 +3,16 @@ var questionElement = document.querySelector(".questions");
 var answerElement = document.querySelector(".answers");
 var startButton = document.querySelector(".start-btn");
 var timerElement = document.querySelector(".countdown");
-var resultText = document.querySelector(".results")
+var resultTextContainer = document.querySelector(".results")
 var mainHeader = document.querySelector(".main-header");
 var instructions = document.querySelector(".instructions");
-var scoreInputSection = document.querySelector (".score-page");
 var currentQuestionIndex = 0;
 var instructionSection = document.querySelector(".start")
 var results = document.querySelector(".results")
+var scorePage = document.querySelector(".score-page");
+var score = 0;
+var scoreDisplay = document.querySelector(".scoredisplay")
+var timeLeft = 30;
 
 //This is the questions variable that is holding all the quiz questions and answer options//
 var questions = [
@@ -45,6 +48,8 @@ var questions = [
    
 ]
 
+
+
 startButton.addEventListener("click", function(){
     countdown();
     showQuestionsAndAnswers();
@@ -55,10 +60,8 @@ function showQuestionsAndAnswers(){
     instructionSection.setAttribute('class', 'hide')
     questionElement.setAttribute('class', 'start')
     answerElement.setAttribute('class', 'start')
-    //results.setAttribute('class', 'start')
     var currentQuestion = questions[currentQuestionIndex];
     questionElement.innerHTML = currentQuestion.question;
-    currentQuestionIndex++
     resetState()
 
     var resultText = document.getElementById("result-text");
@@ -68,67 +71,69 @@ function showQuestionsAndAnswers(){
         var answerButton = document.createElement("button");
         answerElement.appendChild(answerButton);
         answerButton.innerHTML = answer;
-        answerButton.addEventListener("click", answerClick);
+        answerButton.addEventListener("click", answerClick);{
+            if(currentQuestionIndex > 4){
+                scorePage.setAttribute('class', 'start')
+            }
+        }
     })
     //console.log(`correct==> ${questions[currentQuestionIndex].correct}`)
     //console.log(`event ==> ${event.target.textContent}`)
 }
 
 function answerClick(event){
-    var buttonElement = event.target;
-    if(buttonElement.innerhHTML === questions[currentQuestionIndex -1].correct){
-        var resultText = document.getElementById("result-text");
-        resultText.textContent = "Answer is correct!"
+    var resultText = document.getElementById("result-text");
+    var buttonElement =event.target;
+    if(buttonElement.innerHTML !== questions[currentQuestionIndex].correct){
+        resultText.innerText = "Answer is incorrect!"
+        timeLeft = timeLeft - 10;
     }
-    showQuestionsAndAnswers();
+    else{
+        resultText.innerText = "Answer is correct!"
+        score = score + 10;
+        scoreDisplay.textContent = "Your score is " + score;
+    }
+
+    setTimeout(() =>{
+        currentQuestionIndex++
+        showQuestionsAndAnswers()
+    }, 1000)
+    
 }
+
+
 function countdown(){
-    var timeLeft = 60;
     var timeInterval = setInterval(function(){
         if (timeLeft > 1){
             timerElement.textContent = `Time: ` + timeLeft + ' seconds';
             timeLeft--;
-        }else if(timeLeft === 1) {
+        }
+        else if(timeLeft === 1) {
             timerElement.textContent = `Time: ` + timeLeft + ' second';
             timeLeft--;
-        }else {
-            timerElement.textContent = " ";
-            clearInterval(timeInterval);
         }
-    }, 1000);
-    
-}
+        else{timerElement.textContent = " ";
+            clearInterval(timeInterval);
+            questionElement.setAttribute('class', 'hide')
+            answerElement.setAttribute('class', 'hide')
+            scorePage.setAttribute('class', 'start') 
+        }
+        if(currentQuestionIndex > 4){
+            questionElement.setAttribute('class', 'hide')
+            answerElement.setAttribute('class', 'hide')
+            scorePage.setAttribute('class', 'start')    
+        }
+    }, 1000
+)}
 function resetState(){
-    //questionElement.removeChild(questionElement.firstChild)
     while(answerElement.firstChild){
         answerElement.removeChild(answerElement.firstChild)
     }
 }
 
-//function nextQuestion(){
-//    
-//    var currentQuestion = questions[currentQuestionIndex];
-//    questionElement.innerHTML = currentQuestion.question;
-
-//    currentQuestion.answers.forEach(answer=>{
-//        var answerButton = document.createElement("button");
-//        answerElement.appendChild(answerButton);
-//        answerButton.innerHTML = answer.text;
-//        answerButton.addEventListener("click", showNextQuestion);{
-           
-//      }
-//    });   
-// }
-
-
-//function showNextQuestion (event){
-//    //currentQuestionIndex++
-//    if(currentQuestionIndex<questions.length){
-//        nextQuestion();
-//    }
-//    if(event.target.textContent === questions[currentQuestionIndex].correct){
-//        resultText.textContent = "Correct!"
-//    }
-    
-// console.log(`correct==> ${questions[currentQuestionIndex].correct}`)
-//    console.log(`event ==> ${event.target.textContent}`)
+function resetStateLast(){
+    questionElement.removeChild(questionElement.firstChild)
+    while(answerElement.firstChild){
+        answerElement.removeChild(answerElement.firstChild)
+    }
+}
